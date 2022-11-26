e8c5bdcfedfb2f6b1223728f7ce7691b974e076d
TatianaKokunina <tanechka.91@mail.ru>
2022-11-26 18:26:18 +0300

    DZ

--- EX27/Program.cs
+++ EX27/Program.cs
@@ -5,16 +5,16 @@
 */
 Console.WriteLine("Введите число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
-int SumNumber(int numberN){
-    
-    int counter = Convert.ToString(numberN).Length;
-    int product = 0;
-    int result = 0;
-
-    for (int i = 0; i < counter; i++){
-      product = numberN - numberN % 10;
-      result = result + (numberN - product);
-      numberN = numberN / 10;
+int SumNumber(int numberN)
+{
+  int counter = Convert.ToString(numberN).Length;
+  int product = 0;
+  int result = 0;
+  for (int i = 0; i < counter; i++)
+  {
+    product = numberN - numberN % 10;
+    result = result + (numberN - product);
+    numberN = numberN / 10;
     }
    return result;
   }
---
+++ EX29/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] CreateArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int N = 8;
+int min = 0;
+int max = 99;
+int[]Array = CreateArray(N, min, max);
+PrintArray(Array);
